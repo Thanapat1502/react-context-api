9e6730d1c0d74d67d095b9e109b3298daf1b8b4f
Thanapat1502 <psy150201027@gmail.com>
2024-11-01 13:39:01 +0700

    feat: add Context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import React from "react";
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+export const UserDataContext = React.createContext();
+/**
+ * 
+ *  ให้นำข้อมูล username จาก Object userData ที่อยู่ใน Component App มา Render ลงตรง (x) ของข้อความ “สวัสดีคุณ (x)” ด้วย React Context API
+
+ให้นำข้อมูล level จาก Object userData ที่อยู่ใน Component App มา Render ลงตรง (x) ของข้อความ “คุณคือสมาชิกในระดับ (x) ...” ด้วย React Context API
+ */
 
 function App() {
   const userData = {
@@ -10,15 +17,16 @@ function App() {
     avatar: "https://placedog.net/100/100",
     level: "platinum",
   };
-
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
+      <UserDataContext.Provider value={userData}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
